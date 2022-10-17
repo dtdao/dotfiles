a72992c3761318e117a56f76382ea5dfb1e33c52
Dong Dao <kwalitycontrol@gmail.com>
2022-10-17 11:58:30 +0900

    Update cmp-nvim-lsp to default_capabilities

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,6 +1,6 @@
 require("treesitter.treesitter")
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local telescope_builtin = require('telescope.builtin')
