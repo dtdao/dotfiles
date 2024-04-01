652328d4a3c7b0a73f2161e20f9afa41e874b977
dtdao <kwalitycontrol@gmail.com>
2024-04-01 09:56:34 +0900

    Update and fix tree shitter

--- nvim/lua/treesitter/treesitter.lua
+++ nvim/lua/treesitter/treesitter.lua
@@ -1,8 +1,17 @@
+require('treesitter-context').setup{
+    enable = true
+}
 vim.g.loaded_ts_context_commentstring = true
+
 require'nvim-treesitter.configs'.setup {
+    sync_install = true,
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "go" },
     highlight = { enable = true },
+    auto_install = true,
     incremental_selection = { enable = true },
     textobjects = { enable = true },
-    -- context_commentstring = { enable = true}
+    context_commentstring = { enable = true}
 }
+
 require("ts_context_commentstring").setup{}
+
